923eaa5462052c836897c5c6d7668edbfdc0aacb
Blesa Patricia <blesapati@gmail.com>
2017-04-20 13:05:59 +0200

    feat(most_common_word) create controller function to call the warning message

--- most-common-word/most_common_word.py
+++ most-common-word/most_common_word.py
@@ -4,6 +4,32 @@
 # When no argument is provided print out
 # No source provided
 # When the argument provided and the source is a file
-# count all words in the given file and print the most common 
-# ("cat", "CAT", "cat," "cat." are different words ) 
+# count all words in the given file and print the most common
+# ("cat", "CAT", "cat," "cat." are different words )
 
+import sys
+
+def print_description():
+    print("usage: python most_common_word.py [source]")
+
+def warning_message():
+    print("No source provided")
+
+def count_words(file_name):
+    try:
+        text = open(file_name , "r")
+        words_to_count = text.read()
+        num_of_words = 0
+        text.close()
+
+    except FileNotFoundError:
+            return 0
+
+
+def controller(arg):
+    if len(sys.argv) == 1:
+        warning_message()
+    else:
+        print_description()
+
+controller(sys.argv)
