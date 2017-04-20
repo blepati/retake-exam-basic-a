157f3cf0cc36a6b069fa0c083efcc0349d96db24
Blesa Patricia <blesapati@gmail.com>
2017-04-20 11:31:25 +0200

    feat(min_max_diff) the function can handle empty list

--- minmaxdiff/min_max_diff.py
+++ minmaxdiff/min_max_diff.py
@@ -1,13 +1,20 @@
 # Create a function called `min_max_diff` that takes a list of numbers as parameter
 # and returns the difference between maximum and minimum values in the list
 # Create basic unit tests for it with at least 3 different test cases
-nums = [5, 7, 9, 15, 21]
+nums = [5, 7, 9, 15, 22]
 
 def min_max_diff(nums):
+    if nums == []:
+        return 0
     max_num = 0
+    min_num = nums[0]
     for num in nums:
         if num > max_num:
             max_num = num
-    return max_num
-    
+    for num in nums:
+        if min_num > num:
+            min_num = num
+    return max_num - min_num
+
+
 print(min_max_diff(nums))
