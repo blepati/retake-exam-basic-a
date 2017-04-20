5b29b2327a46f08aae1d5ebc8b871066b33438eb
Blesa Patricia <blesapati@gmail.com>
2017-04-20 11:13:35 +0200

    feat(min_max_diff) the function can find the biggest number

--- minmaxdiff/min_max_diff.py
+++ minmaxdiff/min_max_diff.py
@@ -1,3 +1,13 @@
 # Create a function called `min_max_diff` that takes a list of numbers as parameter
 # and returns the difference between maximum and minimum values in the list
 # Create basic unit tests for it with at least 3 different test cases
+nums = [5, 7, 9, 15, 21]
+
+def min_max_diff(nums):
+    max_num = 0
+    for num in nums:
+        if num > max_num:
+            max_num = num
+    return max_num
+    
+print(min_max_diff(nums))
